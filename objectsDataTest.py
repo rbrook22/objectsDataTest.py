64203125f921e5768da2e33cadf54211f3ae77b0
Richard Brooks <rickbrooks22@gmail.com>
2019-07-15 11:26:38 -0400

    created test questions for knowledge

---
+++ objectsDataTest.py
@@ -0,0 +1,53 @@
+#Answer these 3 questions without typing code. Then type code to check your answer.
+#What is the value of the expression 4 * (6 + 5)
+
+# #What is the value of the expression 4 * 6 + 5 
+
+# #What is the value of the expression 4 + 6 * 5 
+
+# What is the type of the result of the expression 3 + 1.5 + 4?
+
+# What would you use to find a number’s square root, as well as its square?
+
+# Given the string 'hello' give an index command that returns 'e'. Enter your code in the cell below:
+
+# s = 'hello'
+# # Print out 'e' using indexing
+# Reverse the string 'hello' using slicing:
+
+# s ='hello'
+# # Reverse the string using slicing
+# Given the string hello, give two methods of producing the letter 'o' using indexing.
+
+# s ='hello'
+# # Print out the 'o'
+# # Method 1:
+
+# # Method 2:
+
+# Build this list [0,0,0] two separate ways.
+# # Method 1:
+
+# # Method 2:
+
+# Reassign 'hello' in this nested list to say 'goodbye' instead:
+# list3 = [1,2,[3,4,'hello']]
+
+# Sort the list below:
+# list4 = [5,3,4,6,1]
+
+# Using keys and indexing, grab the 'hello' from the following dictionaries:
+# d = {'simple_key':'hello'}
+# # Grab 'hello'
+# d = {'k1':{'k2':'hello'}}
+# # Grab 'hello'
+# # Getting a little tricker
+# d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+
+# #Grab hello
+# # This will be hard and annoying!
+# d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
+
+# Use a set to find the unique values of the list below:
+
+# list5 = [1,2,2,33,4,4,11,22,3,3,2]
